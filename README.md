984ccb6d2bb32be66a04172ecf7167f3dbea67b8
Peter Barnes <barnespeter213@gmail.com>
2014-04-26 17:18:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Swapzapp Printing Service
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwapzappPrintservice::Application.config.secret_key_base = '07e59a82dd1630c1a83e24f59e5caae45227a45b50e10d9d90f3bb66c3151732a19e6e7d8215f0b7f998a3eed7a25495b5d33099ee4b7271caa44c416aeb7bf2'
